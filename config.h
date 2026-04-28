10a2b2cd89e656315630114be972b6be85c2854c
joyous.fart <endermanari11@gmail.com>
2026-04-27 19:44:32 -0500

    Update config.h
    added examples in config.h

--- config.h
+++ config.h
@@ -1,10 +1,10 @@
 #define SERVER "irc.libera.chat"
 #define PORT   "6667" // lmao 67
-#define NICK   "john_c_larp"
-#define USERNAME "john_c_larpington"
-#define REALNAME "Arius Morgan"
+#define NICK   "foobar"
+#define USERNAME "foobarington"
+#define REALNAME "larp larpington"
 #define PASS   "your_password_here"
 #define FG_COLOR COLOR_WHITE
 #define BG_COLOR COLOR_BLUE
-#define MOTD   "WELCOME TO THE PISSHOLE"
+#define MOTD   "WELCOME TO THE pipes"
 #define AUTOJOIN "#test"
